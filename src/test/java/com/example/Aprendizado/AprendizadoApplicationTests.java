95a1cb75a419ff99f72c52ac9cdedd11a0b9dd21
Gustavo Ramalho <gustavo.ramalho@ftssjcd06.fotosensores.local>
2018-03-28 17:21:54 -0300

    ok

---
+++ src/test/java/com/example/Aprendizado/AprendizadoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.Aprendizado;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AprendizadoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
